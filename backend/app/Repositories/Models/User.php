9dead83573507e5d956e175d52bc2bc281071e53
Rodrigo Ferreira <rodrigo.ferreira@mobly.com.br>
2020-06-10 19:54:23 -0300

    extended base model and added a methods to get and add friends refs

--- backend/app/Repositories/Models/User.php
+++ backend/app/Repositories/Models/User.php
@@ -5,19 +5,41 @@ namespace App\Repositories\Models;
 
 use Illuminate\Support\Collection;
 
-class User
+class User extends BaseModel
 {
+    protected $refs = [ 'friends' => [] ];
     private $friends = array();
     public $name;
     public $email;
     public $username;
 
     /**
+     * User constructor.
+     * @param $name
+     * @param $email
+     * @param $username
+     */
+    public function __construct($name, $email, $username)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->username = $username;
+    }
+
+    /**
      * @return Collection
      */
-    public function getFriends()
+    public function getFriends(): Collection
     {
         return collect($this->friends);
     }
 
+
+    public function addFriend($username)
+    {
+        // The idea is that the model saves references as a form of relationship,and the outer repository resolve them
+        // to $this->friends collection, on demand
+        
+        return $this->addRef('friends','username',$username);
+    }
 }
