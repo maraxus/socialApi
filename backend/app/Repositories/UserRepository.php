fd43c2959f651535033beaabc9aee42b55793e36
Rodrigo Ferreira <rodrigo.ferreira@mobly.com.br>
2020-06-10 19:53:37 -0300

    Added user repository W.I.P

---
+++ backend/app/Repositories/UserRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories;
+use Illuminate\Support\Collection;
+use App\Repositories\Support\Contracts\ModelFactoryContract as Factory;
+
+class UserRepository
+{
+    protected $userCollection;
+    protected $userFactory;
+
+    public function __construct(Factory $factory)
+    {
+        $this->userFactory = $factory->spawn(6);
+    }
+
+    public function getUserWithRefs(string $lookup,string $ref): Collection
+    {
+        return collect([]);
+    }
+
+    public function addUserNewFriend(string $username, string $friendUsername): bool
+    {
+        $user = $this->userCollection->firstWhere('username',$username);
+        $user->addFriend($friendUsername);
+        return true;
+    }
+
+}
