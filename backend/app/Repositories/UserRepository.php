8d758293505902b748fe0193ddbd55902837442e
Rodrigo Ferreira <rodrigo.ferreira@mobly.com.br>
2020-06-11 17:56:08 -0300

    created a methods to resolve refs into collections inside the models, and added temporary test data to the constructor.

--- backend/app/Repositories/UserRepository.php
+++ backend/app/Repositories/UserRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Repositories;
+use foo\bar;
 use Illuminate\Support\Collection;
 use App\Repositories\Support\Contracts\ModelFactoryContract as Factory;
 
@@ -12,22 +13,38 @@ class UserRepository
     public function __construct(Factory $factory)
     {
         $this->userFactory = $factory;
-        $this->userCollection = collect($factory->spawn(6));
+        $this->userCollection = collect($this->userFactory->spawn(6));
+        $barney = $this->userFactory->testUser('Barney');
+        $Ted = $this->userFactory->testUser('Ted');
+        $Marshall = $this->userFactory->testUser('Marshall');
+        $this->userCollection->push($barney);
+        $this->userCollection->push($Ted);
+        $this->userCollection->push($Marshall);
     }
 
-    public function getUserWithRefs(string $lookup,string $ref)
+    public function getUserWithRefs(string $username, string $ref)
     {
-        $user = $this->userCollection->firstWhere('username',$lookup);
+        $user = $this->userCollection->firstWhere('username',$username);
         if (!$user) return false;
-        $user->resolveRes($ref);
+        $user = $this->resolveRef('friends', $user);
         return $user;
     }
 
     public function addUserNewFriend(string $username, string $friendUsername)
     {
         $user = $this->userCollection->firstWhere('username',$username);
-        if (!$user) return false;
         $user->addFriend($friendUsername);
+        $user = $this->resolveRef('friends',$user);
+        return $user;
+    }
+
+    protected function resolveRef(string $refName, $user)
+    {
+        return $user;
+        $ref = $user->getRefs()[$refName];
+        if (!$ref || count($ref) < 1) return false;
+        $search = $this->userCollection->whereIn($ref['attr'],$ref['matchers']);
+        $user->{ "set{$ref['refAttr']}" }($search);
         return $user;
     }
 
